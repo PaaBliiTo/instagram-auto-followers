34c4ba6541dac96bb5e99f86496c6516e84e5843
Fabio Benoit <fabio@bitonic.nl>
2017-07-25 16:06:43 +0200

    Removed useless code + updated readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Instagram-auto-followers
-A node-based program to get new free legit followers 
+
+> A node-based program to get new free legit followers and likes
+
+## Context/How it works
+
+On Instagram, a lot of people just follow you or like your media when you had a first activity on their account (like or subscribe).
+This program will simply like a random recent media from some random popular hashtag you can find in the hashtags.json file (which you can change according to your needs) every 1 to 2 minutes.
+
+All of these "random" stuffs are to prevent getting detected by Instagram. In addition, you have every call 1.77% chance to have a 5min break ans 0.58% of chance to have a 10min break.
+
+The process might be quite slow and you should not expect hundreds of new likes or subscribes everyday, the point is that you can just leave it running endlessly without worrying about getting banned by Instagram for spamming (at least, I never experienced this). Subscribers will come anyway.
+
+## Install
+
+1. Clone/download the repository
+2. If you still don't have it : [install node](https://nodejs.org/)
+3. Open folder in the terminal
+4. Run `npm install` to install the package instagram-private-api from [huttarichard](https://github.com/huttarichard)
+5. Copy the file credentials.json.example to credentials.json and type in your username and password for instagram
+6. Run `npm start` and leave it running
+
+Note : The console will display the username of the user who posted the media you just liked, and also "5min sleep" and "10min sleep" if they happen
--- start.js
+++ start.js
@@ -4,35 +4,20 @@ var storage = new Client.CookieMemoryStorage();
 var credentials = require('./credentials.json')
 var hashtags = require('./hashtags.json');
 
-var followed = []
-var d = new Date();
-var startTime = d.getTime()
-
 Client.Session.create(device, storage, credentials.username, credentials.password).then(function(data){
     like(data)
-    follow(data)
 })
 
 function like(session){
 
-    var t = new Date();
-    var actualTimeLike = t.getTime()
-    if((actualTimeLike - startTime) > 43200000){
-        var rand = (Math.random()*(14400000-10800000)+10800000)*1000
-        console.log("3 to 4h sleep")
-        d = new Date()
-        startTime = d.getTime()
+    var rand = (Math.random()*(120-60)+60)*1000
+    if(rand > 118000){
+        rand = (Math.random()*(360-300)+300)*1000
+        console.log("5min sleep")
     }
-    else{
-        var rand = (Math.random()*(120-60)+60)*1000
-        if(rand > 118000){
-            rand = (Math.random()*(360-300)+300)*1000
-            console.log("5min sleep")
-        }
-        else if(rand > 119500){
-            rand = (Math.random()*(600-500)+500)*1000
-            console.log("10min sleep")
-        }
+    else if(rand > 119500){
+        rand = (Math.random()*(600-500)+500)*1000
+        console.log("10min sleep")
     }
     
 
@@ -45,49 +30,6 @@ function like(session){
             user = media.account.id;
             Client.Like.create(session, media.id)
         })
-
-        // setTimeout(unfollow, 1200000, session)
         like(session)
     }, rand)
-}
-
-function follow(session){
-    var y = new Date();
-    var actualTimeFollow = y.getTime()
-    if((actualTimeFollow - startTime) > 43200000){
-        var rand = (Math.random()*(14400000-10800000)+10800000)*1000
-        console.log("3 to 4h sleep")
-        d = new Date()
-        startTime = d.getTime()
-    }
-    else {
-        var rand = (Math.random()*(1800000-1200000)+1200000)*1000
-    }
-
-    setTimeout(function(){
-        var tag = hashtags[Math.floor(Math.random()*hashtags.length)]
-        var tagMedia = new Client.Feed.TagMedia(session, tag);
-        tagMedia.get().then(function(data){
-            var media = data[0]
-            user = media.account.id;
-            Client.Relationship.create(session, user)
-            console.log("follow")
-            // followed.push(user) 
-        })
-
-        follow(session)
-    }, rand)
-}
-
-function unfollow(session){
-    Client.Relationship.get(session, followed[0]).then(function(data){
-        if(data.params.followedBy == false){
-            Client.Relationship.destroy(session, followed[0]);
-            // console.log('unfollowed');
-        }
-        else{
-            console.log('not unfollowed');
-        }
-        followed.splice(0,1);
-    });
 }
